af92059ec2d1458ffd28d90723bdd85de1ed2a00
BurmistrovYuA <burmistrov.yu.a@yandex.ru>
2022-08-17 15:21:18 +0300

    Добавлены тесты PointTests

--- sandbox/src/main/java/ru/stqa/Java_SoftwareTesting/sandbox/MySecondProgram.java
+++ sandbox/src/main/java/ru/stqa/Java_SoftwareTesting/sandbox/MySecondProgram.java
@@ -4,7 +4,7 @@ public class MySecondProgram {
 
   public static void main(String[] args) {
     Point p1 = new Point(0,0);
-    Point p2 = new Point(1,1);
+    Point p2 = new Point(0,2);
     //System.out.println("Расстояние между точкой p1=(" + p1.x +","+p1.y+ ") и p2=("+ p2.x +","+p2.y+ ") ="+ Point.distance(p1,p2));
     System.out.println("Расстояние между точкой p1=(" + p1.x +","+p1.y+ ") и p2=("+ p2.x +","+p2.y+ ") ="+ p1.distance(p2));
   }
---
+++ sandbox/src/test/java/ru/stqa/Java_SoftwareTesting/sandbox/PointTests.java
@@ -0,0 +1,24 @@
+package ru.stqa.Java_SoftwareTesting.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testDistance1() {
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(0, 2);
+
+    Assert.assertEquals(p2.distance(p1), 2.0);
+  }
+
+  @Test
+  public void testDistance2() {
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(0, 3);
+
+    Assert.assertEquals(p2.distance(p1), 2.0);
+  }
+
+}
