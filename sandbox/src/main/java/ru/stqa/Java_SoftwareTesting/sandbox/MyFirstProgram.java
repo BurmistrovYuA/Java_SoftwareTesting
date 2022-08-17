773adfbe9216b9df37a4650d8720fed484c534b8
BurmistrovYuA <burmistrov.yu.a@yandex.ru>
2022-08-17 10:02:59 +0300

    Изменение привязки проекта к репозиторию Git

--- sandbox/src/main/java/ru/stqa/Java_SoftwareTesting/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/Java_SoftwareTesting/sandbox/MyFirstProgram.java
@@ -1,9 +1,9 @@
 package ru.stqa.Java_SoftwareTesting.sandbox;
 
 public class MyFirstProgram {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello, world");
-	}
-	
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+
 }
