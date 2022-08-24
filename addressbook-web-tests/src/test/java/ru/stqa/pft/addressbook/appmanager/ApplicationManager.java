eb57d249cfe7cb04ddb0b92ff1abbaa9207b4bf1
BurmistrovYuA <burmistrov.yu.a@yandex.ru>
2022-08-24 22:25:26 +0300

    Созданы NavigationHelper, SessionHelper, GroupHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,75 +1,19 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.time.Duration;
 
 public class ApplicationManager {
-  public WebDriver wd;
+  WebDriver wd;
 
-  public void init() {
-    wd = new ChromeDriver();
-    wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-    wd.get("http://localhost/addressbook/edit.php");
-    login("admin", "secret");
-  }
+  private SessionHelper sessionHelper;
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
 
-  public void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
-
-  public void returnTogroupPage() {
-    wd.findElement(By.linkText("groups")).click();
-  }
-
-  public void submitGroupCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  public void fillGroupForm(GroupData groupData) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wd.findElement(By.xpath("//form[@action='/addressbook/group.php']")).click();
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  public void initGroupCreation() {
-    wd.findElement(By.name("new")).click();
-  }
-
-  public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
-  }
-
-  public void stop() {
-    wd.findElement(By.linkText("Logout")).click();
-    wd.quit();
-  }
-
-  public boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
+/*
   public boolean isAlertPresent() {
     try {
       wd.switchTo().alert();
@@ -77,13 +21,41 @@ public class ApplicationManager {
     } catch (NoAlertPresentException e) {
       return false;
     }
+  }*/
+
+  public void init() {
+    wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    wd.get("http://localhost/addressbook/edit.php");
+    groupHelper = new GroupHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    sessionHelper = new SessionHelper(wd);
+    sessionHelper.login("admin", "secret");
   }
 
-  public void deleteSelectedGroups() {
-    wd.findElement(By.name("delete")).click();
+
+
+  public void stop() {
+    wd.findElement(By.linkText("Logout")).click();
+    wd.quit();
+  }
+/*
+  public boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }*/
+
+
+
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
   }
 
-  public void selectGroup() {
-    wd.findElement(By.name("selected[]")).click();
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteGroupTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/DeleteGroupTest.java
@@ -6,9 +6,9 @@ public class DeleteGroupTest extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
-    app.gotoGroupPage();
-    app.selectGroup();
-    app.deleteSelectedGroups();
-    app.returnTogroupPage();
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().deleteSelectedGroups();
+    app.getGroupHelper().returnToGroupPage();
   }
 }
