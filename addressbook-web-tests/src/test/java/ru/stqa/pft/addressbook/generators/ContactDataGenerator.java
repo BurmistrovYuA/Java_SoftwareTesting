f80ed03ff803fee734986134c89f521ed970c655
BurmistrovYuA <burmistrov.yu.a@yandex.ru>
2022-10-06 16:17:27 +0300

    Задание 12 Реализовать генераторы тестовых данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -3,6 +3,9 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.io.File;
@@ -17,9 +20,9 @@ public class ContactDataGenerator {
   @Parameter(names = "-c",  description="Contact count")
   public int count;
   @Parameter(names = "-f",  description="Target file")
-  public String file;/*
-  @Parameter(names = "-n",  description="Data format")
-  public String format;*/
+  public String file;
+  @Parameter(names = "-d",  description="Data format")
+  public String format;
 
   public static void main(String[] args) throws IOException {
     ContactDataGenerator generator = new ContactDataGenerator();
@@ -35,14 +38,29 @@ public class ContactDataGenerator {
 
   private void run() throws IOException {
     List<ContactData> contacts = generateContact(count);
-    save(contacts, new File(file));
+    if (format.equals("csv")) {
+      saveAsCsv(contacts, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXml(contacts, new File(file));
+    } else if (format.equals("json")){
+      saveAsJson(contacts, new File(file));
+    } else {
+      System.out.println("Unrecognize format" + format);
+    }
+  }
+  private  void saveAsXml(List<ContactData> contacts, File file) throws IOException {
+    XStream xstream= new XStream();
+    xstream.processAnnotations(ContactData.class);
+    String xml=xstream.toXML(contacts);
+    Writer writer =new FileWriter(file);
+    writer.write(xml);
+    writer.close();
   }
 
-
-  private static void save(List<ContactData> contacts, File file) throws IOException {
+  private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (ContactData contact : contacts){
-      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(),
+      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n",contact.getFirstname(),
               contact.getMiddlename(),contact.getLastname(),contact.getNickname(),
               contact.getCompany(),contact.getAddress(),contact.getHomePhone(),
               contact.getMobilePhone(),contact.getWorkPhone(),contact.getEmail(),
@@ -50,8 +68,15 @@ public class ContactDataGenerator {
     }
     writer.close();
   }
+  private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
+    Gson gson= new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json= gson.toJson(contacts);
+    Writer writer =new FileWriter(file);
+    writer.write(json);
+    writer.close();
+  }
 
-  private static List<ContactData> generateContact(int count) {
+  private List<ContactData> generateContact(int count) {
     List<ContactData> contacts = new ArrayList<ContactData>();
     for (int i = 0; i < count; i++){
       contacts.add(new ContactData().
@@ -59,8 +84,8 @@ public class ContactDataGenerator {
               withLastname(String.format("Last_name %s", i)).withNickname(String.format("Nickname %s", i)).
               withCompany(String.format("Company %s", i)).withAddress(String.format("Address %s", i)).
               withHomePhone(String.format("8928552251%s", i)).withMobilePhone(String.format("8928552252%s", i)).
-              withWorkPhone(String.format("8928552253%s", i)).withEmail(String.format("Email%s" + ".ya.ru", i)).
-              withEmail2(String.format("Email2%s" + ".ya.ru", i)).withEmail3(String.format("Email3%s" + ".ya.ru", i)));
+              withWorkPhone(String.format("8928552253%s", i)).withEmail(String.format("Email%s" + "@ya.ru", i)).
+              withEmail2(String.format("Email2%s" + "@ya.ru", i)).withEmail3(String.format("Email3%s" + "@ya.ru", i)));
     }
     return contacts;
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,27 +1,48 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.io.File;
 import java.util.Objects;
+@XStreamAlias("group")
 
 public class ContactData {
+  @XStreamOmitField
   private int id = Integer.MAX_VALUE;;
+  @Expose
   private String name;
+  @Expose
   private String middlename;
+  @Expose
   private String last_name;
+  @Expose
   private String nickname;
+  @Expose
   private String address;
+  @Expose
   private String company;
+  @Expose
   private String allPhones;
+  @Expose
   private String homePhone;
+  @Expose
   private String mobilePhone;
+  @Expose
   private String workPhone;
+  @Expose
   private String email;
+  @Expose
   private String email2;
+  @Expose
   private String email3;
+  @Expose
   private String group;
+  @Expose
   private String allEmails;
+  @Expose
   private File photo;
-
   public File getPhoto() { return photo; }
   public String getAllEmails() { return allEmails;}
   public int getId() { return id; }
@@ -48,7 +69,6 @@ public class ContactData {
   public String getEmail() { return email; }
   public String getEmail2() { return email2; }
   public String getEmail3() { return email3; }
-
   public String getGroup() { return group; }
 
   public ContactData withId(int id) {
